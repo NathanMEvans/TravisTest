aedd724815146f21376e8e388c2b0d821a2919f5
NathanMEvans <nathan@touchfantastic.com>
2016-02-18 08:54:24 +0000

    Initial Commit

---
+++ TravisTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TravisTest
+//
+//  Created by Nathan Evans on 18/02/2016.
+//  Copyright © 2016 TouchFantasticLtd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
